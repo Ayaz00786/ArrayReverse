eb5bc2f669b9bf05594d003732f344d4180a5336
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-26 12:04:39 +0530

    Add project files.

---
+++ ArrayReverse/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayReverse
+{
+    internal class Class1
+    {
+        public static void Array()
+        {
+            int[] array = new int[5];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter number in Position {0}:", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Value Stored in Array Are: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" {0} ", array[i]);
+            }
+
+            Console.WriteLine("\n\nReverse Number of Array :");
+            for (int i = array.Length-1; i >=0; i--)
+            {
+                Console.Write(" {0} ",array[i]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
